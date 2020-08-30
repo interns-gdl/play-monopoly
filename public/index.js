758af6648935b0eda3c233764796e5d05e3d8c3f
Dario Martinez <gusdamtzr@gmail.com>
2020-08-29 22:27:53 -0500

    Navbar navigation done
    Each time the user selects one page in the navbar, the selected page is loaded

--- public/index.js
+++ public/index.js
@@ -12,5 +12,55 @@ function main(){
 
 //this function will after the waiting room
 function start(){
-    $('main').html('It begins!')
+    if (!admin){
+        $('#settings').remove();
+    }
+    $('.nav-item').click(e=>{
+        if(!$(e.currentTarget).hasClass('active')){
+            $('.nav-item').removeClass('active');
+            $(e.currentTarget).addClass('active');
+            load($(e.currentTarget).html());
+        }
+    });
+    loadTransactions();
+}
+
+function load(page){;
+    resetPage();
+    switch(page){
+        case 'Transactions':
+            loadTransactions();
+            break;
+        case 'My account':
+            loadMyAccount();
+            break;
+        case 'Pay':
+            loadPay();
+            break;
+        case 'Settings':
+            loadSettins();
+            break;
+        default:
+            break;
+    }
+}
+
+function resetPage(){
+    $('main').empty();
+}
+
+function loadTransactions(){
+    $('main').html('These are the transactions!');
+}
+
+function loadMyAccount(){
+    $('main').html('These is your account!');
+}
+
+function loadPay(){
+    $('main').html('These is where you pay others!');
+}
+
+function loadSettings(){
+    $('main').html('These are the settings!');
 }
